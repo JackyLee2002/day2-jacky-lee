245984c33df14d12ad22d6750d5a4f5c7a80e406
Jacky Lee <leeja12@oocl.com>
2024-11-19 16:52:09 +0800

    test: given orient west x=0, should be at x=-1, when move forward

--- src/test/java/org/example/MarsRoverTest.java
+++ src/test/java/org/example/MarsRoverTest.java
@@ -110,7 +110,7 @@ public class MarsRoverTest {
 
 
     @Test
-    public void should_move_forward_when_orient_north() { //    move forward from 00N to 01N
+    public void should_be_at_0_1_N_when_move_forward_when_orient_north() { //    move forward from 00N to 01N
         // given
         MarsRover rover = new MarsRover();
         // when
@@ -120,7 +120,7 @@ public class MarsRoverTest {
     }
 
     @Test
-    public void should_move_forward_when_orient_east() { //    move forward from 00E to 10E
+    public void should_be_at_1_0_E_when_move_forward_when_orient_east() { //    move forward from 00E to 10E
         // given
         MarsRover rover = new MarsRover();
         rover.executeCommand("R");
@@ -131,7 +131,7 @@ public class MarsRoverTest {
     }
 
     @Test
-    public void should_move_forward_when_orient_south() { //    move forward from 00S to 0-1S
+    public void should_be_at_0_negative1_S_move_forward_when_orient_south() { //    move forward from 00S to 0-1S
         // given
         MarsRover rover = new MarsRover();
         rover.executeCommand("R");
@@ -140,6 +140,18 @@ public class MarsRoverTest {
         String response = rover.executeCommand("M");
         // then
         assertEquals("0:-1:S", response);
+    }
+
+//    west
+    @Test
+    public void should_be_at_negative1_0_W_when_move_forward_when_orient_west() { //    move forward from 00W to -10W
+        // given
+        MarsRover rover = new MarsRover();
+        rover.executeCommand("L");
+        // when
+        String response = rover.executeCommand("M");
+        // then
+        assertEquals("-1:0:W", response);
     }
 
 
