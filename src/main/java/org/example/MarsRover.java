a1df3d51ebc0cdda9280f13d23e02d0999a53005
Jacky Lee <leeja12@oocl.com>
2024-11-19 15:38:29 +0800

    fix: wrong commit

--- src/main/java/org/example/MarsRover.java
+++ src/main/java/org/example/MarsRover.java
@@ -1,8 +1,5 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.Objects;
-
 public class MarsRover {
     private String direction;
 
@@ -18,17 +15,15 @@ public class MarsRover {
         return "0:0:" + this.direction;
     }
 
-    public void turnLeft() {
-        System.out.println(direction);
-        int index = (Arrays.asList(directions).indexOf(direction) - 1 + directions.length) % directions.length;
-        direction = directions[index];
-        System.out.println(direction);
-    }
-
     public String executeCommand(String command) {
-        if ("L".equals(command)) {
-            turnLeft();
+        if(direction.equals("N")) {
+            direction = "W";
+            return "0:0:" +direction;
         }
-        return showStatus();
+        if (direction.equals("W")) {
+            direction = "S";
+            return "0:0:" + direction;
+        }
+        return "0:0:" + direction;
     }
 }
