f4ab86db9b64c0273eaeb637759cf46417436bd3
Jacky Lee <leeja12@oocl.com>
2024-11-19 15:45:35 +0800

    feat: implement turnLeft command

--- src/main/java/org/example/MarsRover.java
+++ src/main/java/org/example/MarsRover.java
@@ -7,22 +7,32 @@ public class MarsRover {
         this.direction = "N";
     }
 
-    String[] directions = {"N", "E", "S", "W"};
-
-
-
     public String showStatus() {
         return "0:0:" + this.direction;
     }
 
-    public String executeCommand(String command) {
-        if(direction.equals("N")) {
-            direction = "W";
-            return "0:0:" +direction;
+//    use switch case to handle turn left
+    public void turnLeft() {
+         switch (direction) {
+             case "N":
+                direction = "W";
+                 break;
+            case "W":
+                direction = "S";
+                break;
+            case "S":
+                direction = "E";
+                break;
+            case "E":
+                direction = "N";
+             default:
+                 break;
         }
-        if (direction.equals("W")) {
-            direction = "S";
-            return "0:0:" + direction;
+    }
+
+    public String executeCommand(String command) {
+        if (command.equals("L")) {
+            turnLeft();
         }
         return "0:0:" + direction;
     }
